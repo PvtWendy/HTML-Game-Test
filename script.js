a82fdeb7d2a43e0720cd5ee0deb914289dcc0606
PvtWendy <wendyhemlock@gmail.com>
2022-05-04 13:57:43 -0300

    Updating so I don't have to call innerHTML every function

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 let amount = 0
+let clickStrength = 1
 let slaveCost = 10
 let slaveAmount = 0
 document.getElementById('mainBtn').addEventListener('click', addAmount)
-document.getElementById('amount').innerHTML = Math.round(amount * 10) / 10
-document.getElementById('slaveCost').innerHTML = slaveCost
-document.getElementById('slaveAmount').innerHTML = slaveAmount
 document.getElementById('buySlave').addEventListener('click', addSlave)
+
 function addAmount() {
-  amount += 1
-  document.getElementById('amount').innerHTML = Math.round(amount * 10) / 10
+  amount += clickStrength
 }
 function addSlave() {
   if (amount >= slaveCost) {
     slaveAmount += 1
     amount -= slaveCost
     slaveCost = Math.round(slaveCost * 1.15)
-    document.getElementById('amount').innerHTML = Math.round(amount * 10) / 10
-    document.getElementById('slaveAmount').innerHTML = slaveAmount
-    document.getElementById('slaveCost').innerHTML = slaveCost
   }
+} 
+function autoAmount() {
+  amount += slaveAmount / 100
 }
+/*----------------------------------------------------------------------------------
+Game loop
+----------------------------------------------------------------------------------*/
+
+//Generates passive income per 0.1 sec
 setInterval(() => {
-  amount += slaveAmount / 10
+  autoAmount()
+}, 100)
+//Update every millisecond the interface
+setInterval(() => {
   document.getElementById('amount').innerHTML = Math.round(amount * 10) / 10
-}, 1000)
+  document.getElementById('slaveAmount').innerHTML = slaveAmount
+  document.getElementById('slaveCost').innerHTML = slaveCost
+}, 1);

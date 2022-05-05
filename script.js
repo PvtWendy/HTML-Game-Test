5e8a8b1795e334c81ddefb6cc2e658dfca310917
PvtWendy <wendyhemlock@gmail.com>
2022-05-04 22:42:12 -0300

    Added some quality of life changes for adding future buttons, added buttons, ignored CSS

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 let amount = 0
 let clickStrength = 1
-let slaveCost = 10
-let slaveAmount = 0
+let factoryCost = [10,50,100,250,500,1000,2500,5000,10000]
+let factoryAmount = [0,0,0,0,0,0,0,0]
+let cont = 0
 document.getElementById('mainBtn').addEventListener('click', addAmount)
-document.getElementById('buySlave').addEventListener('click', addSlave)
 
 function addAmount() {
   amount += clickStrength
 }
-function addSlave() {
-  if (amount >= slaveCost) {
-    slaveAmount += 1
-    amount -= slaveCost
-    slaveCost = Math.round(slaveCost * 1.15)
+//To be honest, I don't know why this didn't glitch badly, but
+//holy shit am I thankful it didn't
+function addFactories() {
+  if (amount >= factoryCost[cont]) {
+    factoryAmount[cont] += 1
+    amount -= factoryCost[cont]
+    factoryCost[cont] = Math.round(factoryCost[cont] * 1.15)
   }
-} 
+}
+  buySlow.onclick = function addSlow() {
+    cont = 0
+    addFactories();
+  }
+  buyClicker.onclick = function addClicker() {
+    cont = 1
+    addFactories();
+  }
+  buyFast.onclick = function addFast() {
+    cont = 2
+    addFactories();
+  }
+/*Also add the amounts here because... well also couldn't think
+ another way to avoid this terribleness*/ 
 function autoAmount() {
-  amount += slaveAmount / 100
+  amount += factoryAmount[0] / 100
+  amount += factoryAmount[1] / 50
+  amount += factoryAmount[2] / 10
 }
 /*----------------------------------------------------------------------------------
 Game loop
@@ -26,9 +44,13 @@ Game loop
 setInterval(() => {
   autoAmount()
 }, 100)
-//Update every millisecond the interface
+//Update the interface every millisecond
 setInterval(() => {
   document.getElementById('amount').innerHTML = Math.round(amount * 10) / 10
-  document.getElementById('slaveAmount').innerHTML = slaveAmount
-  document.getElementById('slaveCost').innerHTML = slaveCost
+  document.getElementById('slowAmount').innerHTML = factoryAmount[0]
+  document.getElementById('slowCost').innerHTML = factoryCost[0]
+  document.getElementById('clickerCost').innerHTML = factoryCost[1]
+  document.getElementById('clickerAmount').innerHTML = factoryAmount[1]
+  document.getElementById('fastCost').innerHTML = factoryCost[2]
+  document.getElementById('fastAmount').innerHTML = factoryAmount[2]
 }, 1);
